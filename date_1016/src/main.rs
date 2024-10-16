5131355903e4a2faf297f27a1e73c324327e2341
Stonestonestone <liulichuqing@outlook.com>
2024-10-16 21:59:24 +0800

    001

--- date_1016/src/main.rs
+++ date_1016/src/main.rs
@@ -1,3 +1,19 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+    fn z(&self) -> &T {
+        &self.y
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {},p.z = {}", p.x(),p.z());
 }
